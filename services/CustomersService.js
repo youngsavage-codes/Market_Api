a4d8063a75ef345a8957e1fbd92b8df1a199b6e3
youngsavage-codes <158312694+CodingSavage1@users.noreply.github.com>
2024-08-11 12:49:14 +0100

    update

--- services/CustomersService.js
+++ services/CustomersService.js
@@ -1,5 +1,5 @@
 const Customers = require("../model/customersModel");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 class CustomersService {
